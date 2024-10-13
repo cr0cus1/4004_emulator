3d77667a9ad57d1614e439dc5ee37b8b4a0941ac
silver_punk <silvercrocus@proton.me>
2024-10-13 20:45:08 +0200

    tbc

--- main.c
+++ main.c
@@ -4,23 +4,23 @@
 
 #define LINE_LEN 50
 
-int is_label(const char *first_word) {
-    if(first_word[strlen(first_word) - 2] == ',')
-        return 0;
-    return 1;
-}
+//void is_label(const char *first_word) {
+//    printf("%c\n", first_word[strlen(first_word)-2]);
+//    if(first_word[strlen(first_word) - 1] == ',')
+//        return 0;
+//    return 1;
+//}
 
 void print_func() {
     printf(" > ");
 }
 
-char *first_literal(const char *input_line) {
+void split_input_line(const char *input_line) {
     char *first_word = malloc(20*sizeof(char)); // let's assume that max length of first literal can be max 20 bytes(in 4-bit assembler lmao xD)
     for(int i = 0; input_line[i] != ' '; i++)
         first_word[i] = input_line[i];
     first_word[strlen(first_word)] = '\0';
 
-    return first_word;
 }
 
 int count_spaces(const char *input_line) {
@@ -36,10 +36,11 @@ int count_spaces(const char *input_line) {
 }
 
 void parse_line(const char *input_line) {
-    char *first_word;
-    first_word = first_literal(input_line);
-    printf("%s\n", first_word);
-    free(first_word);
+    char *first_literal= malloc(20*sizeof(char)); // let's assume that max length of first literal can be max 20 bytes(in 4-bit assembler lmao xD)
+
+    printf("%s\n", first_literal);
+    free(first_literal);
+    first_literal = NULL;
 }
 
 int main(int argc, char **argv) {
