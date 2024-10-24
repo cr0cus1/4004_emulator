0477a1c1243abeed6147e77956428cd95af5b816
silver_punk <silvercrocus@proton.me>
2024-10-24 13:19:21 +0200

    Define 4004's cpu structure.

--- 4004.h
+++ 4004.h
@@ -1,6 +1,13 @@
 #ifndef EMULATOR_H_
 #define EMULATOR_H_
 
+typedef struct {
+    int index_register[16];
+    int program_counter;
+    int stack[3];
+    int adder;
+} cpu_struct;
+
 int init_cpu_env();
 void instruction_cycle();
 
